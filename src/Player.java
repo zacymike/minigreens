bf4d8a5745ed1e39cda8b584f0ce61eee6e52f1b
PeterSturm <sturm.peti@gmail.com>
2016-03-26 12:19:03 +0100

    Update turn method

--- src/Player.java
+++ src/Player.java
@@ -10,7 +10,20 @@ public class Player implements Movable
     public Player(){}
     public void die(){}
     public void step(){}
-    public void turn(TurnDirection dir){}
+    public void turn(TurnDirection dir)
+    {
+        switch(direction)
+        {
+            case NORTH: if(dir == TurnDirection.LEFT) direction = Direction.WEST; else direction = Direction.EAST;
+                break;
+            case EAST: if(dir == TurnDirection.LEFT) direction = Direction.NORTH; else direction = Direction.SOUTH;
+                break;
+            case SOUTH: if(dir == TurnDirection.LEFT) direction = Direction.EAST; else direction = Direction.WEST;
+                break;
+            case WEST: if(dir == TurnDirection.LEFT) direction = Direction.SOUTH; else direction = Direction.NORTH;
+                break;
+        }
+    }
     public void pickUp(){}
     public void putDown(){}
 }
