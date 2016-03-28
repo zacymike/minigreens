88c07a8b961f2183fb9b4d19bf84c86bda68d6f0
zacymike <h.mark.hun@gmail.com>
2016-03-28 23:31:52 +0200

    logging added to Box, Gap, Scale

--- src/Box.java
+++ src/Box.java
@@ -14,17 +14,27 @@ public class Box implements Element
     @Override
     public void interact(Player player)
     {
+        Logger.enter(this.getClass(), "interact()", player.getClass());
 
+        Logger.exit(this.getClass(), "interact()", player.getClass());
     }
 
     @Override
     public void interact(Bullet bullet)
     {
+        Logger.enter(this.getClass(), "interact()", bullet.getClass());
+
         bullet.destroy();
+
+        Logger.exit(this.getClass(), "interact()", bullet.getClass());
     }
 
     public void destroy()
     {
+        Logger.enter(this.getClass(), "destroy()", null);
+
         alive = false;
+
+        Logger.exit(this.getClass(), "destroy()", null);
     }
 }
--- src/Gap.java
+++ src/Gap.java
@@ -7,15 +7,25 @@ public class Gap implements Element {
 
     @Override
     public void interact(Player player) {
+        Logger.enter(this.getClass(), "interact()", player.getClass());
+
         player.die();
+
+        Logger.exit(this.getClass(), "interact()", player.getClass());
     }
 
     @Override
     public void interact(Bullet bullet) {
+        Logger.enter(this.getClass(), "interact()", bullet.getClass());
 
+        Logger.exit(this.getClass(), "interact()", bullet.getClass());
     }
 
     public void interact(Box box) {
+        Logger.enter(this.getClass(), "interact()", box.getClass());
+
         box.destroy();
+
+        Logger.exit(this.getClass(), "interact()", box.getClass());
     }
 }
