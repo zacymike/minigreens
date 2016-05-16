e0da07d158c82b3492ff26bfcd684cdf10a8e5cf
zacymike <h.mark.hun@gmail.com>
2016-05-16 22:49:39 +0200

    weightlimit-handling implemented in Scale

--- src/Scale.java
+++ src/Scale.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Scale implements Element
 {
     private Door door;
     private int limit;
     private int current_w;
-    private ArrayList<Weight> weights;
+    private Stack<Box> boxes;
 
     public Scale(Door door)
     {
         this.door = door;
+        boxes = new Stack<>();
     }
 
     @Override
@@ -20,11 +22,13 @@ public class Scale implements Element
     {
         Logger.enter(this.getClass(), "interact()", player.getClass());
 
-        // ha van doboz a merlegen (nyitva az ajto), akkor az ezredes nem tud ralepni
-        if (!door.isOpened()) {
-            door.open();
-            player.setPosition(player.getPosition().getNeighbour(player.getDirection()));
-        }
+        // jatekos ralep, suly hozzaadodik, ha van nala doboz, akkor annak is
+        player.setPosition(player.getPosition().getNeighbour(player.getDirection()));
+        current_w += player.getWeight();
+        if (player.getBox() != null) current_w += player.getBox().getWeight();
+
+        // ha a sulykorlatot atlepjuk, az ajto kinyilik
+        if(current_w >= limit) door.open();
 
         Logger.exit(this.getClass(), "interact()", player.getClass());
     }
@@ -44,7 +48,14 @@ public class Scale implements Element
     {
         Logger.enter(this.getClass(), "interact()", box.getClass());
 
-        door.open();
+        // doboz hozzaadasa a veremhez, sulyanak hozzaadasa a szamlalohoz
+        boxes.push(box);
+        current_w += box.getWeight();
+
+        // ha a sulykorlatot atlepjuk, az ajto kinyilik
+        if(current_w >= limit) {
+            door.open();
+        }
 
         Logger.exit(this.getClass(), "interact()", box.getClass());
     }
@@ -54,6 +65,18 @@ public class Scale implements Element
     {
         Logger.enter(this.getClass(), "pickedUp()", player.getClass());
 
+        // ha nincs rajta doboz, nem tortenik semmi
+        if (boxes.size() != 0) {
+            // ha van, akkor a player felveszi a dobozt, sulyat levonjuk a szamlalobol
+            player.setBox(boxes.pop());
+            current_w -= player.getBox().getWeight();
+
+            // ha sulykorlat ala csokken a szamlalo, az ajto bezarul (ha nyitva volt)
+            if (door.isOpened() && (limit > current_w)) {
+                door.close();
+            }
+        }
+
         Logger.exit(this.getClass(), "pickedUp()", player.getClass());
     }
 
@@ -62,7 +85,11 @@ public class Scale implements Element
     {
         Logger.enter(this.getClass(), "steppedOut()", player.getClass());
 
-        door.close();
+        // player sulyanak levonasa, ha sulykorlat ala kerulunk, bezarjuk az ajtot (ha nyitva volt)
+        current_w -= player.getWeight();
+        if(door.isOpened() && (limit > current_w)) {
+            door.close();
+        }
 
         Logger.exit(this.getClass(), "steppedOut()", player.getClass());
     }
@@ -71,8 +98,6 @@ public class Scale implements Element
     public void steppedOut(Box box)
     {
         Logger.enter(this.getClass(), "steppedOut()", box.getClass());
-
-        door.close();
 
         Logger.exit(this.getClass(), "steppedOut()", box.getClass());
     }
@@ -95,19 +120,5 @@ public class Scale implements Element
 
     public int getCurrent_w() {
         return current_w;
-    }
-
-    public void addCurrentw(int s){
-        this.current_w += s;
-    }
-
-    public Weight getWeight(int i)
-    {
-        return weights.get(i);
-    }
-
-    public void putWeight(Weight w)
-    {
-        weights.add(w);
     }
 }
